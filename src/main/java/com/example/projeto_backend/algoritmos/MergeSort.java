27259fdc2bc8c84cc3fef94ab2361aa2eaa7c2de
AdlerCastro <adlercastro01@gmail.com>
2024-12-02 00:28:59 -0300

    :sparkles: feat: Lógica Merge Sort

---
+++ src/main/java/com/example/projeto_backend/algoritmos/MergeSort.java
@@ -0,0 +1,44 @@
+package com.example.projeto_backend.algoritmos;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static int[] sort(int[] array) {
+        if (array.length <= 1) {
+            return array;
+        }
+        int mid = array.length / 2;
+        int[] left = Arrays.copyOfRange(array, 0, mid);
+        int[] right = Arrays.copyOfRange(array, mid, array.length);
+
+        left = sort(left);
+        right = sort(right);
+
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+
+        return result;
+    }
+}
+
